21745f87229bc49d8936ab36cb4b485d18a647d9
isaiahzs <isaiahzs@uchicago.edu>
2021-05-06 15:38:28 -0500

    Add movement stuff

--- src/main/java/testmod/testmod/entity/CubeEntity.java
+++ src/main/java/testmod/testmod/entity/CubeEntity.java
@@ -1,9 +1,13 @@
 package testmod.testmod.entity;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.PathAwareEntity;
+import testmod.testmod.Testmod;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -21,9 +25,20 @@ public class CubeEntity extends AnimalEntity {
         super(entityType, world);
     }
 
+    protected void initGoals() {
+        this.goalSelector.add(0, new SwimGoal(this));
+        this.goalSelector.add(1, new EscapeDangerGoal(this, 2.0D));
+        this.goalSelector.add(2, new AnimalMateGoal(this, 1.0D));
+        this.goalSelector.add(3, new TemptGoal(this, 1.25D, Ingredient.ofItems(Items.WHEAT), false));
+        this.goalSelector.add(4, new FollowParentGoal(this, 1.25D));
+        this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0D));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(7, new LookAroundGoal(this));
+    }
+
     @Nullable
     @Override
     public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
-        return null;
+        return Testmod.CUBE.create(world);
     }
 }
